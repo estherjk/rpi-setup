e12f508f7f9ed0a913a318a04e723a84f32a6cf1
Esther Jun Kim <esther.jun.kim@gmail.com>
2018-01-20 07:32:50 -0800

    Replace hard-coded SD card path by prompting user
    This makes it more flexible, especially if the SD card path is different on another user's PC.

--- scripts/copy-to-boot.sh
+++ scripts/copy-to-boot.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-# Update path to SD card (/Volumes/boot) if it's different on your PC
-rsync -rv ../boot/ /Volumes/boot/
+# Prompt user for input
+read -p "Enter SD card path (e.g. /Volumes/boot): " sd_card_path
+
+echo "Copying files to SD card..."
+rsync -rv ../boot/ $sd_card_path
